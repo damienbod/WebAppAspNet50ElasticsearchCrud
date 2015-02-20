33cd64fd72a901a50f76800eabb469a3079d886f
damienbod <damien_bod@hotmail.com>
2015-02-20 23:49:44 +0100

    init

---
+++ src/WebAppAspNet50ElasticsearchCrud/Controllers/SearchController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using WebAppAspNet50ElasticsearchCrud.SearchProvider;
+
+namespace WebAppAspNet50ElasticsearchCrud.Controllers
+{
+    public class SearchController : Controller
+    {
+		readonly ISearchProvider _searchProvider = new ElasticSearchProvider();
+
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(Skill model)
+		{
+			if (ModelState.IsValid)
+			{
+				model.Created = DateTime.UtcNow;
+				model.Updated = DateTime.UtcNow;
+				_searchProvider.AddUpdateEntity(model);
+
+				return Redirect("Search/Index");
+			}
+
+			return View("Index", model);
+		}
+
+		[HttpPost]
+		public ActionResult Update(long updateId, string updateName, string updateDescription)
+		{
+			_searchProvider.UpdateSkill(updateId, updateName, updateDescription);
+			return Redirect("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Delete(long deleteId)
+		{
+			_searchProvider.DeleteSkill(deleteId);
+			return Redirect("Index");
+		}
+
+		public JsonResult Search(string term)
+		{
+			return Json(_searchProvider.QueryString(term));//, "Skills", JsonRequestBehavior.AllowGet);
+		}
+
+		public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
