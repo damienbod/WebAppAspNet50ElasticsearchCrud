b78777adbf263075e277b4f366c88332f010541c
damienbod <damien_bod@hotmail.com>
2015-11-24 18:17:31 +0100

    upgrading ElasticsearchCrud and ASP.NET 5

--- src/WebAppAspNet50ElasticsearchCrud/Startup.cs
+++ src/WebAppAspNet50ElasticsearchCrud/Startup.cs
@@ -1,63 +1,39 @@
-﻿using System;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Routing;
-using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using WebAppAspNet50ElasticsearchCrud.Providers;
-
-namespace WebAppAspNet50ElasticsearchCrud
+﻿namespace WebAppAspNet50ElasticsearchCrud
 {
-	public class Startup
-	{
-		// This method gets called by the runtime.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			// Add MVC services to the services container.
-			services.AddMvc();
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
 
-			services.AddTransient<ISearchProvider, SearchProvider>();
-			// Uncomment the following line to add Web API servcies which makes it easier to port Web API 2 controllers.
-			// You need to add Microsoft.AspNet.Mvc.WebApiCompatShim package to project.json
-			// services.AddWebApiConventions();
-		}
+    using WebAppAspNet50ElasticsearchCrud.Providers;
 
-		// Configure is called after ConfigureServices is called.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
-		{
-			// Configure the HTTP request pipeline.
-			// Add the console logger.
-			loggerfactory.AddConsole();
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
 
-			// Add the following to the request pipeline only in development environment.
-			if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
-			{
-				app.UseBrowserLink();
-				app.UseErrorPage(ErrorPageOptions.ShowAll);
-			}
-			else
-			{
-				// Add Error handling middleware which catches all application specific errors and
-				// send the request to the following path or controller action.
-				app.UseErrorHandler("/Search/Error");
-			}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
 
-			// Add static files to the request pipeline.
-			app.UseStaticFiles();
+            services.AddScoped<ISearchProvider, SearchProvider>();
+        }
 
-			// Add MVC to the request pipeline.
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller}/{action}/{id?}",
-					defaults: new { controller = "Search", action = "Index" });
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
 
-				// Uncomment the following line to add a route for porting Web API 2 controllers.
-				// routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
-			});
-		}
-	}
+            app.UseIISPlatformHandler();
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
 }
