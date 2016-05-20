645774a3940276e91486b768e8a93b5003362f40
damienbod <damien_bod@hotmail.com>
2016-05-20 20:06:18 +0200

    Updating the ASP.NET Core RC2 and ElasticsearchCrud RC2

--- src/WebAppAspNet50ElasticsearchCrud/Controllers/SearchController.cs
+++ src/WebAppAspNet50ElasticsearchCrud/Controllers/SearchController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using WebAppAspNet50ElasticsearchCrud.Providers;
 
--- src/WebAppAspNet50ElasticsearchCrud/Startup.cs
+++ src/WebAppAspNet50ElasticsearchCrud/Startup.cs
@@ -1,40 +1,72 @@
-﻿namespace WebAppAspNet50ElasticsearchCrud
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using WebAppAspNet50ElasticsearchCrud.Providers;
+
+namespace AngularPlotlyAspNetCore
 {
-    using Microsoft.AspNet.Builder;
-    using Microsoft.AspNet.Hosting;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-
-    using WebAppAspNet50ElasticsearchCrud.Providers;
-
     public class Startup
     {
         public Startup(IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add Cors support to the service
+            services.AddCors();
+
+            var policy = new Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicy();
+
+            policy.Headers.Add("*");
+            policy.Methods.Add("*");
+            policy.Origins.Add("*");
+            policy.SupportsCredentials = true;
+
+            services.AddCors(x => x.AddPolicy("corsGlobalPolicy", policy));
+
             services.AddMvc();
 
             services.AddScoped<ISearchProvider, SearchProvider>();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.MinimumLevel = LogLevel.Information;
+            app.UseCors("corsGlobalPolicy");
+
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
 
-            app.UseIISPlatformHandler();
+            app.UseDefaultFiles();
 
             app.UseStaticFiles();
-
-            app.UseMvcWithDefaultRoute();
-            //app.UseMvc();
+            app.UseMvc();
         }
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
     }
 }
